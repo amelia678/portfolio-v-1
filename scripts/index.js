dce3fefff45a8595be098597cdf40aab90dd7add
Amelia Schulz <you@example.com>
2018-12-04 18:49:35 -0500

    fixes funky scroll

--- scripts/index.js
+++ scripts/index.js
@@ -30,7 +30,7 @@ var sticky = header.offsetTop;
 function stickyFunction() {
     if (window.pageYOffset > sticky) {
         header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
+    // } else {
+    //     header.classList.remove("sticky");
     }
 }
